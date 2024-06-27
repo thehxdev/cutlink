ff858a0f645e9c0547ae1c30082104c76fe6c489
thehxdev <hossein.khosravi.ce@gmail.com>
2024-06-27 14:48:47 +0330

    use nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         # Use stable branch of nixpkgs
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
     outputs = { self, nixpkgs, ... }: let
